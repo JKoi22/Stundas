85ceeb9948a5a026d4032d229e45d325b5facf18
Janis <janis.koikins@edu.tukums.lv>
2024-10-16 14:22:02 +0300

    mod

--- py10.py
+++ py10.py
@@ -2,4 +2,7 @@ aa=["janvāris","februāris","marts","aprīlis","maijs","jūnijs","jūlijs","aug
 wr=open("menesi2.txt","w")
 for i in aa:
     wr.write(i +"\n")
+wr1=open("menesi2.txt","w")
+print(wr1)
 wr.close()
+wr1.close()
