c40ea294cbbbd855e6f18e948034a1415ff4cec5
Janis <janis.koikins@edu.tukums.lv>
2024-10-16 14:24:10 +0300

    utf

--- py10.py
+++ py10.py
@@ -1,5 +1,5 @@
 aa=["janvāris","februāris","marts","aprīlis","maijs","jūnijs","jūlijs","augusts","septembris","oktobris","novembris","decembris"]
-wr=open("menesi2.txt","w")
+wr=open("menesi2.txt","w", encoding= "utf-8")
 for i in aa:
     wr.write(i +"\n")
 wr1=open("menesi2.txt","w")
