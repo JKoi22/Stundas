134835a062aab027114a6397992d6743af27914f
Janis <janis.koikins@edu.tukums.lv>
2024-10-30 14:58:06 +0200

    bez virsrakstu

--- py16.py
+++ py16.py
@@ -1,8 +1,9 @@
 import csv
 with open("darbinieki.csv","r",encoding= "utf-8") as fails:
     lasītājs=csv.reader(fails, delimiter=",")
+    next(lasītājs)
     for ierak in lasītājs:
         vards=ierak[0]
         nodarb=ierak[3]
         print(f"{vards}-{nodarb}")
-fails.close()
+fails.close()   
