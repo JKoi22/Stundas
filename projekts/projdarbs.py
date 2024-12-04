2cca950f8d782a7b0e8edfa2e941e574716c07b7
Janis <janis.koikins@edu.tukums.lv>
2024-12-04 15:22:26 +0200

    beigas, tagad pa isto

--- projekts/projdarbs.py
+++ projekts/projdarbs.py
@@ -63,12 +63,8 @@ def novpag(pagasts):
         for rinda in lasītājs:
              if rinda[10] == zempag.capitalize():
                   i+=1
-                  if rinda[8]=="":
-                    print("pietura nr.",i,rinda[1])
-                    aa.append(["pietura nr.",i,rinda[1]])
-                  else:
-                    print("pietura nr.",i,rinda[1],", iela -",rinda[8])
-                    aa.append(["pietura nr.",i,rinda[1],", iela -",rinda[8]])
+                  print("pietura nr.",i,rinda[1])
+                  aa.append(["pietura nr.",i,rinda[1]])
      with open(y, "w", newline="", encoding="utf-8") as fails:
                     rakst=csv.writer(fails)
                     rakst.writerow(aa)
@@ -81,17 +77,13 @@ def novciem(ciemats):
         aa=[]
         for rinda in lasītājs:
              if rinda[11] == zemciem.capitalize():
-                  i+=1
-                  if rinda[8]=="":
-                    print("pietura nr.",i,"-",rinda[1])
-                    aa.append(["pietura nr.",i,"-",rinda[1]])
-                  else:
-                    print("pietura nr.",i,"-",rinda[1],", iela -",rinda[8])
-                    aa.append(["pietura nr.",i,"-",rinda[1],", iela -",rinda[8]])
+               i+=1
+               print("pietura nr.",i,"-",rinda[1])
+               aa.append(["pietura nr.",i,"-",rinda[1]])
      with open(y, "w", newline="", encoding="utf-8") as fails:
                rakst=csv.writer(fails)
                rakst.writerow(aa)
-izvēlne=int(input("ievadi skaitli 1-7 1=skaits novadā, 2=skaits pagastā 3=skaits ciemā/pilsētā 4=novada pieturu nosaukumi 5=pagasta pieturu nosaukumi 6=ciema/pilsēta 7= ceļa numurs pieturai"))
+izvēlne=int(input("ievadi skaitli 1-6 1=skaits novadā, 2=skaits pagastā 3=skaits ciemā/pilsētā 4=novada pieturu nosaukumi 5=pagasta pieturu nosaukumi 6=ciema/pilsētas pieturu nosaukumi"))
 if izvēlne==1:
           x=input("ievadi novadu")
           (novskaits(x))
